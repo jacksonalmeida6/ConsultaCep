8130ba8343c8296be6db81fe477bdccb95b02ac2
Jackson almeida de oliveira <jacksonalmeida6@gmail.com>
2020-01-03 02:39:24 -0300

    Primeiro Commit

---
+++ ConsultaCep/ConsultaCep/Servico/ViaCepServico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using ConsultaCep.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace ConsultaCep.Servico
+{
+    public class ViaCepServico
+    {
+        private static string EnderecoURL = "http//viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEndereco = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEndereco);
+
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo) ;
+
+            return end;
+
+        }
+    }
+}
